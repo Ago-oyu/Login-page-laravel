d30b201d855c90b096b60e125f72f51305a6c344
safwandm <safwan.damanik@gmail.com>
2023-12-21 10:18:43 +0700

    Progress dari menit 30:30 sampai akhir.

--- login-page/app/Http/Controllers/AuthManager.php
+++ login-page/app/Http/Controllers/AuthManager.php
@@ -3,14 +3,59 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Session;
+use App\Models\User;
+
 
 class AuthManager extends Controller
 {
     function login() {
+        if (Auth::check()) {
+            return redirect(route('home'));
+        }
         return view('login');
     }
 
     function registration() {
+        if (Auth::check()) {
+            return redirect(route('home'));
+        }
         return view('registration');
+    }
+
+    function loginPost(Request $request) {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended(route('home'));
+        }
+        return redirect(route('login'))->with("error", "Login detail invalid");
+    }
+
+    function registrationPost(Request $request) {
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'password' => 'required'
+        ]);
+        $data['name'] = $request->name;
+        $data['email'] = $request->email;
+        $data['password'] = Hash::make($request->password);
+        $user = User::create($data);
+        if (!$user) {
+            return redirect(route('registration'))->with("error", "Registration detail invalid");
+        }
+        return redirect(route('login'))->with("success", "Registration successful");
+    }
+
+    function logout() {
+        Session::flush();
+        Auth::logout();
+        return redirect(route('login'));
     }
 }
--- login-page/routes/web.php
+++ login-page/routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthManager;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,14 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/login', [AuthManager::class, 'login'])->name('login');
+Route::post('/login', [AuthManager::class, 'loginPost'])->name('login.post');
 
-Route::get('/login', function(){
-    return view('login');
-});
+Route::get('/registration', [AuthManager::class, 'registration'])->name('registration');
+Route::post ('/registration', [AuthManager::class, 'registrationPost'])->name('registration.post');
 
-Route::get('/registration', function(){
-    return view('registration');
+Route::group (['middleware' => 'auth'], function () {
+    Route::get ('/logout', [AuthManager::class, 'logout'])->name('logout');
+    Route::get ('/secret', function () {
+        return "You cannot view this page if you are not logged in";
+    });
+    Route::get('/', function () {
+        return view('welcome');
+    })->name('home');
 });
